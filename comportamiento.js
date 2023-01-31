105702fb1e29682c4b7ec75a21a9e1bc436498f7
Martín Gomez <23gmartin@gmail.com>
2023-01-31 17:15:11 -0300

    Décima versión del Proyecto Final

--- comportamiento.js
+++ comportamiento.js
@@ -1,5 +1,16 @@
+//Mensaje de bienvenida
 function bienvenida(){
     const hoy = new Date()
     document.getElementById("caja_bienvenida").style.display = "block";
     document.getElementById("renglon_bienvenida").innerHTML = hoy.toLocaleDateString();
+}
+
+document.getElementById("foto_perfil").addEventListener("mouseover",agrandar());
+var ancho_foto = document.getElementById("foto_perfil").style.width;
+var alto_foto = document.getElementById("foto_perfil").style.height;
+function agrandar() {
+    ancho_foto += ancho_foto*0.1;
+    alto_foto += alto_foto*0.1;
+    document.getElementById("foto_perfil").style.width = ancho_foto;
+    document.getElementById("foto_perfil").style.height = alto_foto;
 }
